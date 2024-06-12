e522b3ca9920daba65cdbab87b032ae1477a027b
Viktor Rura <ruravictor@gmail.com>
2024-06-12 20:23:56 +0100

    Implemented functionality for the blog. No serching and posting is available as well as deleting

--- src/PostPage.js
+++ src/PostPage.js
@@ -10,10 +10,19 @@ const PostPage = ({ posts, handleDelete }) => {
                     <>
                         <h2>{post.title}</h2>
                         <p className="postDate">{post.datetime}</p>
-                        <p className="postDate">{post.body}</p>
+                        <p className="postBody">{post.body}</p>
                         <button onClick={() => handleDelete(post.id)}>
                             Delete Post
                         </button>
+                    </>
+                )}
+                {!post && (
+                    <>
+                        <h2>Post Not Found</h2>
+                        <p>Well, that's disappointing.</p>
+                        <p>
+                            <Link to="/">Visit Our Homepage</Link>
+                        </p>
                     </>
                 )}
             </article>
