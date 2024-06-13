8d6f104dfda1e23d6974b2b2a3a73e91c2dc4c1c
Viktor Rura <ruravictor@gmail.com>
2024-06-13 07:19:36 +0100

    Added editing functionality

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import NewPost from "./NewPost";
 import PostPage from "./PostPage";
 import About from "./About";
 import Missing from "./Missing";
+import EditPost from "./EditPost";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { format } from "date-fns";
+import { format, set } from "date-fns";
+import api from "./api/posts";
 
 function App() {
     const [posts, setPosts] = useState([]);
@@ -16,7 +18,28 @@ function App() {
     const [searchResults, setSearchResults] = useState([]);
     const [postTitle, setPostTitle] = useState("");
     const [postBody, setPostBody] = useState("");
+    const [editTitle, setEditTitle] = useState("");
+    const [editBody, setEditBody] = useState("");
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await api.get("/posts");
+                setPosts(response.data);
+            } catch (err) {
+                if (err.response) {
+                    // Not in the 200 response range
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else {
+                    console.log(`Error: ${err.message}`);
+                }
+            }
+        };
+        fetchPosts();
+    }, []);
 
     useEffect(() => {
         const filteredResults = posts.filter(
@@ -28,22 +51,50 @@ function App() {
         setSearchResults(filteredResults.reverse());
     }, [posts, search]);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
         const datetime = format(new Date(), "MMMM dd, yyyy pp");
         const newPost = { id, title: postTitle, datetime, body: postBody };
-        const allPosts = [...posts, newPost];
-        setPosts(allPosts);
-        setPostTitle("");
-        setPostBody("");
-        navigate("/");
+        try {
+            const response = await api.post("/posts", newPost);
+            const allPosts = [...posts, response.data];
+            setPosts(allPosts);
+            setPostTitle("");
+            setPostBody("");
+            navigate("/");
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
     };
 
-    const handleDelete = (id) => {
-        const postsList = posts.filter((post) => post.id !== id);
-        setPosts(postsList);
-        navigate("/");
+    const handleEdit = async (id) => {
+        const datetime = format(new Date(), "MMMM dd, yyyy pp");
+        const updatedPost = { id, title: editTitle, datetime, body: editBody };
+        try {
+            const response = await api.put(`/posts/${id}`, updatedPost);
+            setPosts(
+                posts.map((post) =>
+                    post.id === id ? { ...response.data } : post
+                )
+            );
+            setEditTitle("");
+            setEditBody("");
+            navigate("/");
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            await api.delete(`/posts/${id}`);
+            const postsList = posts.filter((post) => post.id !== id);
+            setPosts(postsList);
+            navigate("/");
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
     };
 
     return (
@@ -52,6 +103,19 @@ function App() {
             <Nav search={search} setSearch={setSearch} />
             <Routes>
                 <Route path="/" element={<Home posts={searchResults} />} />
+                <Route
+                    path="/edit/:id"
+                    element={
+                        <EditPost
+                            posts={posts}
+                            handleEdit={handleEdit}
+                            editTitle={editTitle}
+                            setEditTitle={setEditTitle}
+                            editBody={editBody}
+                            setEditBody={setEditBody}
+                        />
+                    }
+                />
                 <Route
                     path="/post"
                     element={
